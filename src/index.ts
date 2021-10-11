63dcfcf6ad57f7084706be4d941358ee8bf489e9
obedpaz <lesterpaz99@gmail.com>
2021-10-10 23:37:56 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './main.scss';
+
+// Co-Fi ...study, code, and work with LoFi vibes
+// MP3CHIDO
+
+console.log('Hello from TS');
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+/**@type {import('webpack').Configuration} */
+module.exports = {
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x)?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: 'body',
+			template: './public/index.html',
+			filename: 'index.html',
+		}),
+		new MiniCssExtractPlugin(),
+		new CopyPlugin({
+      patterns: [
+        { from: "./src/media/", to: "dist" },
+      ],
+    }),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		compress: true,
+		port: 8080,
+		open: true
+	},
+};
